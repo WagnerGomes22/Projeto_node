e75631d85818996334cd73d2fbc645f01b5b00af
Wagner-tech3 <wagnergomesdossantosw5@gmail.com>
2024-03-13 12:48:40 -0300

    att-responsivo

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router(); // obejto de rotas do express
 const job = require('../models/jobs');
+const jobs = require('../models/jobs');
 
 
 // rota de teste 
@@ -8,6 +9,19 @@ router.get('/test', (req, res) => {
     res.send("deu certo");
 });
 
+// detalhe da vaga -> view/1, view/2
+router.get('/view/:id', (req, res) => jobs.findOne({
+    where: { id: req.params.id }
+}).then(job => {
+
+    res.render('view', {
+        job
+    });
+
+}).catch(err => console.log(err)));
+
+
+// form da rota de view
 router.get('/add', (req, res) => {
     res.render('add');
 });
